30cd326aaea4de85736bb95541644c592b78c1ec
Rafael Ferreira <61996187+rafael3021734@users.noreply.github.com>
2023-05-25 02:28:28 -0300

    second commit

--- app/application.py
+++ app/application.py
@@ -11,49 +11,76 @@ def lambda_handler(event, context):
     for record in event.get('Records'):
         print(event)
         body = json.loads(record.get('body'))
-        contrato = body.get("contrato")
-        parcela = body.get("parcela")
-        print(f' body: {body}')
-        print(f' Contrato: {contrato}')
-        lista = []
-        listaPar= []
+        if 'md5' in body:
+            md5_value = body['md5']
+            print(f'body: 10, md5: {md5_value}')
+        else:
+            contrato = body.get("contrato")
+            parcela = body.get("parcela")
+            print(f' body: {body}')
+            print(f' Contrato: {contrato}')
+            lista = []
+            listaPar = []
 
-        for cont in contrato:
-            objectContrato = Contrato.Contrato(
-            NumContrato= cont['NumContrato'],
-            Serie=cont['Serie'],
-            id=cont['id'],
-            dataContratacao=(cont['dataContratacao'])
-            )
-            lista.append(objectContrato.__dict__)
-        #listaPar.append(parcela)
-        #parce = parcelaLista(parcela)
-        parce = Parcela.Parcela.parcelaLista(parcela)
-    #    for par in parcela:
-    #        parcela = Parcela.Parcela(
-    #            NumContrato=par['NumContrato'],
-    #            Parcela=par['Parcela'],
-    #            qtdParcela=par['qtdParcela']
-    #        )
-    #        listaPar.append(parcela.__dict__)
+            my_list = [1, 2, 3, 4, 5]
+            double_list = map(Contrato.Contrato.double, my_list)
+            print(list(double_list))
+     #       mapobjet = map(Contrato.Contrato.contractImpl, [("1", "1", "1", "1")])
+     #       print(f' map: {list(mapobjet)}')
 
-        data = Data.Data(lista, listaPar)
-        data = Data.Data(lista, parce)
-        print(f'lista:{lista}')
-        #print(f'parcela:{listaPar}')
-        print(f'parcela:{parce}')
-        print(f'Objeto: {objectContrato.__dict__}')
-        print(f'data:{data.__dict__}')
-        #print(f'Objeto: {objectContrato.__dict__}')
+            for cont in contrato:
+                objectContrato = Contrato.Contrato(
+                    NumContrato=cont['NumContrato'],
+                    Serie=cont['Serie'],
+                    id=cont['id'],
+                    dataContratacao=(cont['dataContratacao'])
+                )
+                lista.append(objectContrato.__dict__)
+            # listaPar.append(parcela)
+            # parce = parcelaLista(parcela)
+            parce = Parcela.Parcela.parcelaLista(parcela)
+            #    for par in parcela:
+            #        parcela = Parcela.Parcela(
+            #            NumContrato=par['NumContrato'],
+            #            Parcela=par['Parcela'],
+            #            qtdParcela=par['qtdParcela']
+            #        )
+            #        listaPar.append(parcela.__dict__)
 
-        # id_person = body.get('id')
-        # name = body.get('name')
-        # print(f'Name: {name}, ID:{id_person}')
-        # print(f'teste:{body}')
+            data = Data.Data(lista, listaPar)
+            data = Data.Data(lista, parce)
+            print(f'lista:{lista}')
+            # print(f'parcela:{listaPar}')
+            print(f'parcela:{parce}')
+            print(f'Objeto: {objectContrato.__dict__}')
+            print(f'data:{data.__dict__}')
+            md = "123"
+            #md5 = "md5": md}
+            md5 = json.dumps(md)
+            json_body = json.dumps(body)
+            #json_body = json.dumps(md5)
+            #bodyjson = { body, md5}
+            print(f'body:{{{json_body},"md5":{md5}}}')
+
+            body = {"key1": "value1", "key2": "value2"}
+
+            # Converter o objeto 'body' em uma string JSON formatada
+            json_body = json.dumps(body, indent=4)
+
+            # Exibir a string JSON formatada
+            print(f'body: {json_body}, {md5}')
+
+            # print(f'Objeto: {objectContrato.__dict__}')
+
+            # id_person = body.get('id')
+            # name = body.get('name')
+            # print(f'Name: {name}, ID:{id_person}')
+            # print(f'teste:{body}')
     return {
         'statusCode': 200,
         # 'body': json.dumps('Hello from Lambda!')
     }
+
 
 def parcelaLista(parcela):
     listaPar = []
--- app/teste.py
+++ app/teste.py
@@ -7,6 +7,23 @@ from app.application import lambda_handler
 #record = {'Records': [{'messageId': '58b4580d-1fa2-4249-99bd-cc2b9e5d6f28', 'receiptHandle': 'AQEBtQJ13W80r3WJGpdcMOs/B2G/E117lk9OnJj2lU+/Pw62xiyNmfFCUCfWhmWyb7YJaRj2XZ3dL1C4GoBC89MM/rQ5JeovrKEYJeWIImRTe3sX3Usvdl+IEti684cgwbsEIBMQpNRwv43/0bG8zP4CTzsABmohgAdvKQUjdDcTqaBCKXcShZa5i6dAZx8sk2Szd7DLbbyrq8ukuyOCSJGTMCRA6myHArtRtqvGVqgcQTFLWXJ7maU706cQxus8m3bCVtZo8ErWJx8e1ZvgRFhideclsUQKmunOsyIvoTucX1Iw1qtau6YOq5i2Rp91PEC1PJX22uaVn14aj2IAo3vr3eRbS1pnircfSoZ59KmJkycPaCCt/FvMvmasnhqXfe7z', 'body': '{\n    "contrato": [\n        {\n            "NumContrato": "1221112232",\n            "Serie": 22,\n            "id": "1223",\n            "dataContratacao": "10-10-2022"\n        },\n        {\n            "NumContrato": "22222222",\n            "Serie": 33,\n            "id": "2312",\n            "dataContratacao": "15-01-2022"\n        }\n    ]\n}', 'attributes': {'ApproximateReceiveCount': '1', 'SentTimestamp': '1677304430960', 'SenderId': '669676830092', 'ApproximateFirstReceiveTimestamp': '1677304430970'}, 'messageAttributes': {}, 'md5OfBody': '67dde2c25e5498d73988e1ade245daea', 'eventSource': 'aws:sqs', 'eventSourceARN': 'arn:aws:sqs:us-east-1:669676830092:sqsQueue', 'awsRegion': 'us-east-1'}]}
 record = {'Records': [{'messageId': '6ce49fd0-c169-44e7-909f-3b0ad35b9252', 'receiptHandle': 'AQEBNWki4Z9tjOXHxZUUyamDphWYaMlZNRN22ta826nyTOGa7OpODvf6F9xFfJOnIqB1x5bf22KrdE5OPsEcZwcimDlKzNr0DLndxA+858vGliDhvNGG+wbmRfZ75uqfjXkmQ/bW4JuJuBsB/ajsviI2fZJPA+ZMMYFLxaso8MLRz5GSF9Zf/vjp371sm/1bzZPS3Qfp1M193KWaEDxuGmW8JYKVRP5h39JyH6VwZNdI1Gc5BObMYqWud94kvezv5C5hrx7JHw5h9hfxu2JzFEOQCUzOdL7tDBCiKFOKyPxiMbbxuVJ3DzUxIJYsPe7uZ0fop2C59d8TYli5/07Ez4zaCVQLJwt/yiPaNN59NDKQBMpHtF9lk8VpqZiUPJaXLjjw', 'body': '{\n    "contrato": [\n        {\n            "NumContrato": "1221112232",\n            "Serie": 22,\n            "id": "1223",\n            "dataContratacao": "10-10-2022"\n        },\n        {\n            "NumContrato": "22222222",\n            "Serie": 33,\n            "id": "2312",\n            "dataContratacao": "15-01-2022"\n        }\n    ],\n    "parcela": [\n        {\n            "NumContrato": "1221112232",\n            "Parcela": 22,\n            "qtdParcela": "320"\n        },\n        {\n            "NumContrato": "22222222",\n            "Parcela": 23,\n            "qtdParcela": "320"\n        }\n    ]\n}', 'attributes': {'ApproximateReceiveCount': '1', 'SentTimestamp': '1677463697045', 'SenderId': '669676830092', 'ApproximateFirstReceiveTimestamp': '1677463697059'}, 'messageAttributes': {}, 'md5OfBody': '30de55be8baaf8d8ce6cd51836856f99', 'eventSource': 'aws:sqs', 'eventSourceARN': 'arn:aws:sqs:us-east-1:669676830092:sqsQueue', 'awsRegion': 'us-east-1'}]}
 
+# record = {
+#     'Records': [
+#         {
+#             'messageId': '6ce49fd0-c169-44e7-909f-3b0ad35b9252',
+#             'receiptHandle': 'AQEBNWki4Z9tjOXHxZUUyamDphWYaMlZNRN22ta826nyTOGa7OpODvf6F9xFfJOnIqB1x5bf22KrdE5OPsEcZwcimDlKzNr0DLndxA+858vGliDhvNGG+wbmRfZ75uqfjXkmQ/bW4JuJuBsB/ajsviI2fZJPA+ZMMYFLxaso8MLRz5GSF9Zf/vjp371sm/1bzZPS3Qfp1M193KWaEDxuGmW8JYKVRP5h39JyH6VwZNdI1Gc5BObMYqWud94kvezv5C5hrx7JHw5h9hfxu2JzFEOQCUzOdL7tDBCiKFOKyPxiMbbxuVJ3DzUxIJYsPe7uZ0fop2C59d8TYli5/07Ez4zaCVQLJwt/yiPaNN59NDKQBMpHtF9lk8VpqZiUPJaXLjjw',
+#             'body': ' {\n    "contrato": [\n        {\n            "NumContrato": "1221112232",\n            "Serie": 22,\n            "id": "1223",\n            "dataContratacao": "10-10-2022"\n        },\n        {\n            "NumContrato": "22222222",\n            "Serie": 33,\n            "id": "2312",\n            "dataContratacao": "15-01-2022"\n        }\n    ],\n    "parcela": [\n        {\n            "NumContrato": "1221112232",\n            "Parcela": 22,\n            "qtdParcela": "320"\n        },\n        {\n            "NumContrato": "22222222",\n            "Parcela": 23,\n            "qtdParcela": "320"\n        }\n    ],\n    "md5": "320"\n}',
+#             'attributes': {
+#                 'ApproximateReceiveCount': '1',
+#                 'SentTimestamp': '1677463697045',
+#                 'SenderId': '669676830092',
+#                 'ApproximateFirstReceiveTimestamp': '1677463697059'
+#             },
+#             'messageAttributes': {},
+#             'md5OfBody': '30de55be8baaf8d8ce6cd51836856f99',
+#             'eventSource': 'aws:sqs',
+#             'eventSourceARN': 'arn:aws:sqs:us-east-1:669676830092:sqsQueue',
+#             'awsRegion': 'us-east-1'}]}
 
 
 
